c9bb9a0eb79b858aee18256f035e0a5fed4ce911
ahmad <ahmadimam657@gmail.com>
2021-08-29 18:34:25 +0500

    DAY2COMPLETED

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello World $days"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
+import 'package:dem_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello World"),
-          ),
-        ),
-      ),
+      home: HomePage()
     );
   }
 }
